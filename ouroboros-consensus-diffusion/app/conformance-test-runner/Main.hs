20ab0567133db20097ef42c3735859b90c1d6569
Sandy Maguire <sandy@sandymaguire.me>
2025-11-10 10:22:00 -0800

    downstream

--- ouroboros-consensus-diffusion/app/conformance-test-runner/Main.hs
+++ ouroboros-consensus-diffusion/app/conformance-test-runner/Main.hs
@@ -1,4 +1,8 @@
 module Main (main) where
 
+import Data.Map
+import Ouroboros.Network.Diffusion.Topology (NetworkTopology(..))
+import Test.Consensus.PointSchedule.Peers
+
 main :: IO ()
 main = putStrLn "Hello from the conformance testing of consensus runner executable"
