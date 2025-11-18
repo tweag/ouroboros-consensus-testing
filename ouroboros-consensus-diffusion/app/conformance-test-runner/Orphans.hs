f2b6038d1647d52928e73107160950ec63d955ee
Xavier Góngora <xavier.gongora@tweag.io>
2025-11-18 13:39:51 -0600

    Clean up

--- ouroboros-consensus-diffusion/app/conformance-test-runner/Orphans.hs
+++ ouroboros-consensus-diffusion/app/conformance-test-runner/Orphans.hs
@@ -3,36 +3,31 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
-module Orphans where
+module Orphans () where
 
-import Codec.Serialise
-import Data.Typeable (Typeable)
-import GHC.Generics
-import Ouroboros.Consensus.Block
-import Ouroboros.Consensus.Config.SupportsNode
-import Ouroboros.Consensus.HardFork.Abstract
-import Ouroboros.Consensus.Ledger.Abstract
-import Ouroboros.Consensus.Ledger.CommonProtocolParams
-import Ouroboros.Consensus.Ledger.Inspect
-import Ouroboros.Consensus.Ledger.Query
+import Codec.Serialise (Serialise (decode, encode))
+import GHC.Generics (Generic)
+import Ouroboros.Consensus.Block (Header)
 import Ouroboros.Consensus.Ledger.SupportsMempool
-import Ouroboros.Consensus.Ledger.SupportsPeerSelection
-import Ouroboros.Consensus.Ledger.SupportsProtocol
-import Ouroboros.Consensus.Node.InitStorage
-import Ouroboros.Consensus.Node.NetworkProtocolVersion
-import Ouroboros.Consensus.Node.Run
-import Ouroboros.Consensus.Node.Serialisation
-import Ouroboros.Consensus.Storage.ChainDB
-  ( ImmutableDbSerialiseConstraints
-  , SerialiseDiskConstraints
-  , VolatileDbSerialiseConstraints
+  ( GenTx
+  , GenTxId
+  , TxId
   )
-import Ouroboros.Consensus.Storage.LedgerDB
+import Ouroboros.Consensus.Node.Run
+  ( SerialiseNodeToNodeConstraints (..)
+  )
+import Ouroboros.Consensus.Node.Serialisation
+  ( SerialiseNodeToNode
+  )
 import Ouroboros.Consensus.Storage.Serialisation
-import Ouroboros.Consensus.Util (ShowProxy)
+  ( SerialisedHeader
+  , decodeTrivialSerialisedHeader
+  , encodeTrivialSerialisedHeader
+  )
 import Ouroboros.Network.Block (Serialised)
-import Test.Util.TestBlock
+import Test.Util.TestBlock (TestBlock)
 
 -- * Target instances
 
@@ -47,24 +42,6 @@ instance SerialiseNodeToNode TestBlock (Serialised TestBlock)
 instance SerialiseNodeToNode TestBlock (SerialisedHeader TestBlock)
 instance SerialiseNodeToNode TestBlock (GenTx TestBlock)
 instance SerialiseNodeToNode TestBlock (GenTxId TestBlock)
-
-{- SerialiseNodeToNode class methods
-  encodeNodeToNode :: CodecConfig blk -> BlockNodeToNodeVersion blk -> a -> Encoding
-  decodeNodeToNode :: CodecConfig blk -> BlockNodeToNodeVersion blk -> forall s. Decoder s a
-
-  -- When the config is not needed, we provide a default, unversioned
-  -- implementation using 'Serialise'
-
-  default encodeNodeToNode ::
-    Serialise a =>
-    CodecConfig blk -> BlockNodeToNodeVersion blk -> a -> Encoding
-  encodeNodeToNode _ccfg _version = encode
-
-  default decodeNodeToNode ::
-    Serialise a =>
-    CodecConfig blk -> BlockNodeToNodeVersion blk -> forall s. Decoder s a
-  decodeNodeToNode _ccfg _version = decode
--}
 
 data instance GenTx TestBlock
 data instance TxId (GenTx TestBlock)
