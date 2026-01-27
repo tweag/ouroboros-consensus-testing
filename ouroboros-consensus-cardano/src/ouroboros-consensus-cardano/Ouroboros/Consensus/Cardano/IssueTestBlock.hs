5faa5f8e4f06af56e9d26d1334b0622cf23d8e45
Sandy Maguire <sandy.maguire@tweag.io>
2026-01-27 12:18:53 -0800

    Issue Conway blocks

--- ouroboros-consensus-cardano/src/ouroboros-consensus-cardano/Ouroboros/Consensus/Cardano/IssueTestBlock.hs
+++ ouroboros-consensus-cardano/src/ouroboros-consensus-cardano/Ouroboros/Consensus/Cardano/IssueTestBlock.hs
@@ -8,8 +8,12 @@
 
 module Ouroboros.Consensus.Cardano.IssueTestBlock () where
 
+
 import           Cardano.Crypto.Hash as Hash
 import           Cardano.Crypto.KES as KES
+import           Cardano.Ledger.Alonzo.Tx
+import           Cardano.Ledger.Alonzo.TxAuxData (mkAlonzoTxAuxData)
+import           Cardano.Ledger.Alonzo.TxWits (AlonzoTxWits (..))
 import           Cardano.Ledger.BaseTypes
 import           Cardano.Ledger.Keys hiding (hashVerKeyVRF)
 import           Cardano.Ledger.Shelley.API hiding (hashVerKeyVRF)
@@ -19,28 +23,35 @@ import           Cardano.Protocol.TPraos.BHeader
 import           Cardano.Protocol.TPraos.OCert
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Map as M
-import           Data.Proxy
 import qualified Data.Sequence.Strict as StrictSeq
 import           Ouroboros.Consensus.Cardano.Block (CardanoBlock,
-                     pattern BlockShelley)
+                     pattern BlockConway)
+import           Ouroboros.Consensus.Protocol.Praos.Header
 import           Ouroboros.Consensus.Shelley.Eras
 import           Ouroboros.Consensus.Shelley.Ledger (ShelleyBlock (..),
                      ShelleyHash (..))
-import           Ouroboros.Consensus.Shelley.Protocol.TPraos ()
+import           Ouroboros.Consensus.Shelley.Protocol.Praos ()
 import           Test.Cardano.Ledger.Binary.Random (mkDummyHash)
-import           Test.Cardano.Ledger.Core.KeyPair (KeyPair (..))
-import           Test.Cardano.Ledger.Shelley.Examples.Consensus
+import           Test.Cardano.Ledger.Conway.Examples.Consensus
+import           Test.Cardano.Ledger.Core.KeyPair (KeyPair (..),
+                     mkWitnessesVKey)
+import qualified Test.Cardano.Ledger.Shelley.Examples.Consensus as SLE
 import           Test.Cardano.Ledger.Shelley.Generator.Core
 import           Test.Cardano.Ledger.Shelley.Utils hiding (mkVRFKeyPair)
 import           Test.Consensus.Genesis.Setup.GenChains (IssueTestBlock (..))
 
 
+
 instance IssueTestBlock (CardanoBlock StandardCrypto) where
   issueFirstBlock fork slot = makeCardanoBlock (Just fork) 0 slot Nothing
-  issueSuccessorBlock fork slot (BlockShelley (ShelleyBlock (Block (BHeader (BHBody {bheaderBlockNo, bheaderSlotNo}) _) _) (ShelleyHash hh))) =
-    makeCardanoBlock fork (bheaderBlockNo + 1) (bheadederSlotNo + slot) $ Just $ HashHeader hh
+  issueSuccessorBlock fork slot (BlockConway (ShelleyBlock
+      (Block (Header (HeaderBody {hbBlockNo, hbSlotNo}) _) _)
+      (ShelleyHash hh))) =
+    makeCardanoBlock fork
+      (hbBlockNo + 1)
+      (hbSlotNo + slot) $ Just $ HashHeader hh
   issueSuccessorBlock _ _ _ =
-    -- Impossible because we only ever produce 'BlockShelley' in
+    -- Impossible because we only ever produce 'BlockConway' in
     -- 'makeCardanoBlock'.
     error "issueSuccessorBlock: impossible"
 
@@ -52,53 +63,61 @@ makeCardanoBlock
   -> SlotNo
   -> Maybe HashHeader
   -> CardanoBlock StandardCrypto
-makeCardanoBlock fork blockNo slot mhash = BlockShelley $
+makeCardanoBlock fork blockNo slot mhash = BlockConway $
   let blk =
-        shelleyLedgerBlock slot blockNo mhash
-          (exampleTx
-            (mkWitnessesPreAlonzo (Proxy @ShelleyEra))
-              exampleTxBodyShelley
-              $ forkToAuxData fork)
-    in ShelleyBlock blk $ ShelleyHash $ unHashHeader $ bhHash $ bheader blk
+        conwayLedgerBlock slot blockNo mhash $
+          AlonzoTx
+            exampleTxBodyConway
+            ( AlonzoTxWits
+                (mkWitnessesVKey (hashAnnotated exampleTxBodyConway) [asWitness SLE.examplePayKey]) -- vkey
+                mempty -- bootstrap
+                mempty -- txscripts
+                mempty -- txdats
+                mempty -- redeemers
+            )
+            (IsValid True)
+            ( SJust $ forkToAuxData fork
+            )
+      Block (Header bhb _) _ = blk
+    in ShelleyBlock blk $ ShelleyHash $ castHash $ hbBodyHash bhb
 
 
--- | Construct a made-up (but believable) cardano block for the Shelley era.
-shelleyLedgerBlock ::
+-- | Construct a made-up (but believable) cardano block for the Conway era.
+conwayLedgerBlock ::
   SlotNo ->
   BlockNo ->
   Maybe HashHeader ->
   -- ^ The parent hash, if there is one.
-  Tx ShelleyEra ->
+  Tx ConwayEra ->
   -- ^ Some transaction to stick in the block.
-  Block (BHeader StandardCrypto) ShelleyEra
-shelleyLedgerBlock slot blockNo prev tx = Block blockHeader blockBody
+  Block (Header StandardCrypto) ConwayEra
+conwayLedgerBlock slot blockNo prev tx = Block blockHeader blockBody
   where
     keys :: AllIssuerKeys StandardCrypto 'StakePool
-    keys = exampleKeys
+    keys = SLE.exampleKeys
 
     hotKey = kesSignKey $ snd $ NE.head $ aikHot keys
     KeyPair vKeyCold _ = aikCold keys
 
-    blockHeader :: BHeader StandardCrypto
-    blockHeader = BHeader blockHeaderBody (unsoundPureSignedKES () 0 blockHeaderBody hotKey)
+    blockHeader :: Header StandardCrypto
+    blockHeader = Header blockHeaderBody (unsoundPureSignedKES () 0 blockHeaderBody hotKey)
 
-    blockHeaderBody :: BHBody StandardCrypto
+    blockHeaderBody :: HeaderBody StandardCrypto
     blockHeaderBody =
-      BHBody
-        { bheaderBlockNo = blockNo
-        , bheaderSlotNo = slot
-        , bheaderPrev = maybe GenesisHash BlockHash prev
-        , bheaderVk = coerceKeyRole vKeyCold
-        , bheaderVrfVk = vrfVerKey $ aikVrf keys
-        , bheaderEta = mkCertifiedVRF (mkBytes 0) (vrfSignKey $ aikVrf keys)
-        , bheaderL = mkCertifiedVRF (mkBytes 1) (vrfSignKey $ aikVrf keys)
-        , bsize = 2345
-        , bhash = hashTxSeq blockBody
-        , bheaderOCert = mkOCert keys 0 (KESPeriod 0)
-        , bprotver = ProtVer (natVersion @2) 0
+      HeaderBody
+        { hbBlockNo = blockNo
+        , hbSlotNo = slot
+        , hbPrev = maybe GenesisHash BlockHash prev
+        , hbVk = coerceKeyRole vKeyCold
+        , hbVrfVk = vrfVerKey $ aikVrf keys
+        , hbVrfRes = mkCertifiedVRF (mkBytes 0) (vrfSignKey $ aikVrf keys)
+        , hbBodySize = 2345
+        , hbBodyHash = hashTxSeq blockBody
+        , hbOCert = mkOCert keys 0 (KESPeriod 0)
+        , hbProtVer = ProtVer (natVersion @2) 0
         }
 
-    blockBody = toTxSeq @ShelleyEra (StrictSeq.fromList [tx])
+    blockBody = toTxSeq @ConwayEra (StrictSeq.fromList [tx])
 
     mkBytes :: Int -> Cardano.Ledger.BaseTypes.Seed
     mkBytes = Seed . mkDummyHash @Blake2b_256
@@ -106,8 +125,6 @@ shelleyLedgerBlock slot blockNo prev tx = Block blockHeader blockBody
 
 -- | We need to do something with our fork number to futz the resulting hash.
 -- So we stick it into the auxilliary data that gets attached to the block.
-forkToAuxData :: Maybe Int -> TxAuxData ShelleyEra
-forkToAuxData
-  = ShelleyTxAuxData
-  . foldMap (\i -> M.singleton 1 $ I $ fromIntegral i)
+forkToAuxData :: Maybe Int -> TxAuxData ConwayEra
+forkToAuxData fk = mkAlonzoTxAuxData @_ @ConwayEra (foldMap (M.singleton 1 . I . fromIntegral) fk) []
 
