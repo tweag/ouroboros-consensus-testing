3acfc96b49ea756180685be530cd0e8e51a63d8f
Xavier Góngora <xavier.gongora@tweag.io>
2026-01-29 12:16:30 -0600

    Typos and fixes after first review

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/TestSuite.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/TestSuite.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -23,6 +22,7 @@ import           Data.List.NonEmpty (NonEmpty)
 import qualified Data.List.NonEmpty as NonEmpty
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+import           Data.Universe.Class
 import           Ouroboros.Consensus.Block (BlockSupportsDiffusionPipelining,
                      ConvertRawHash, Header)
 import           Ouroboros.Consensus.Config.SupportsNode (ConfigSupportsNode)
@@ -45,20 +45,33 @@ import qualified Test.Tasty.QuickCheck as QC
 import           Test.Util.TersePrinting (Terse)
 
 data TestSuiteData blk = TestSuiteData
-  { prefix :: NonEmpty Prefix -- ^ The nested groups a test belongs to. The `head` corresponds to the top level.
-  , test   :: ConformanceTest blk -- ^ The test to run
+  { -- | The nested groups a test belongs to.
+    -- It is 'NonEmpty', as we need a test description/name
+    -- in order to produce a 'TestTree'. For this, the
+    -- convention is to use the 'NonEmpty.last' and nest
+    -- the test by appending.
+    prefix :: NonEmpty Prefix
+
+    -- | The test to run.
+  , test   :: ConformanceTest blk
   }
 
 type Prefix = String
 
 newtype TestSuite blk key = TestSuite (Map key (TestSuiteData blk))
-  deriving newtype (Semigroup, Monoid)
 
-mkTestSuite :: Prefix -> key -> ConformanceTest blk -> TestSuite blk key
-mkTestSuite p k t = TestSuite . Map.singleton k $
-  TestSuiteData { prefix = [p]
-                , test = t
-                }
+-- | Smart constructor for a 'TestSuite'.
+mkTestSuite :: (Ord key, Finite key)
+            => (key -> Prefix)
+            -> (key -> ConformanceTest blk)
+            -> TestSuite blk key
+mkTestSuite toDescription toTest = TestSuite . Map.fromList $
+  [ (k, tsData)
+  | k <- universeF
+  , let tsData = TestSuiteData { prefix = [toDescription k]
+                               , test = toTest k
+                               }
+  ]
 
 mapKeys :: Ord b => (a -> b) -> TestSuite blk a -> TestSuite blk b
 mapKeys f (TestSuite m) = TestSuite $ Map.mapKeys f m
@@ -66,7 +79,7 @@ mapKeys f (TestSuite m) = TestSuite $ Map.mapKeys f m
 get :: Ord key => TestSuite blk key -> key ->  ConformanceTest blk
 get (TestSuite m) k = test $ case Map.lookup k m of
   Just t  -> t
-  Nothing -> error "TestSuite.get: Impossible! All test classes have a value."
+  Nothing -> error "TestSuite.get: Impossible! All test classes have a value by contruction."
 
 nest :: [Prefix] -> TestSuite blk key -> TestSuite blk key
 nest pfs (TestSuite m) = TestSuite $
@@ -96,29 +109,31 @@ compileSingleTest ::
   ) =>
   TestSuiteData blk -> ([Prefix], TestTree)
 compileSingleTest (TestSuiteData {prefix, test}) =
-  -- The last element of the prefix corresponds to the individual test
-  -- description/name. So all test matching on `init` are in the same group level.
+  -- The 'last' element of the 'prefix' corresponds to the individual test
+  -- description/name. Conversely, the 'init' corresponds to the enclosing
+  -- test groups.
   let (prefixes,testName) = (NonEmpty.init prefix, NonEmpty.last prefix)
    in (prefixes, QC.testProperty testName (runConformanceTest test))
 
--- | Recursively build test groups by following the hierarchy described by a
--- the 'Prefix' list. The resulting 'TestTrees' are considered to be part
+-- | Recursively build test groups by following the hierarchy described by
+-- the '[Prefix]'. The resulting '[TestTree]' are considered to be part
 -- of the same top level group.
 buildGroups :: [([Prefix], TestTree)] -> [TestTree]
 buildGroups entries =
   let
-    -- Tests that belong directly at this level.
-    (here, deeper) = partition (null . fst) entries
+    -- Distinguish between tests that belong to the top level
+    -- from the nested ones.
+    (toplevel, nested) = partition (null . fst) entries
 
     topLevelTests :: [TestTree]
-    topLevelTests = fmap snd here
+    topLevelTests = fmap snd toplevel
 
-    -- Group the deeper tests by their first prefix
+    -- Group the nested tests by their first prefix.
     grouped :: Map Prefix [([Prefix], TestTree)]
     grouped =
       Map.fromListWith (<>)
         [ (p, [(ps, t)])
-        | (p:ps, t) <- deeper
+        | (p:ps, t) <- nested
         ]
 
     subgroups :: [TestTree]
