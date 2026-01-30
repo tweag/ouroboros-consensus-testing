f19861b745b26ab271fa3603c109434c25b17b41
Xavier Góngora <xavier.gongora@tweag.io>
2026-01-30 09:57:38 -0600

    Second review fixes and revamp approach

--- ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/TestSuite.hs
+++ ouroboros-consensus-diffusion/test/consensus-test/Test/Consensus/Genesis/TestSuite.hs
@@ -1,32 +1,37 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE UndecidableInstances #-}
 
--- | A 'TestSuite' data structure to arrange 'ConformanceTest' groups
--- designed to interface between property test execution and the
+-- | A 'TestSuite' data structure for quick access to 'ConformanceTest' valuesa
+-- providing facilities to arrange them in nested hierarchical groups.
+-- It's purpose is interfacing between property test execution and the
 -- conformance testing harness.
--- They are modeled after, and can be compiled to, a tasty 'TestTree'.
 module Test.Consensus.Genesis.TestSuite (
-    TestSuite
+    Generic
+  , GenericUniverse (..)
+  , TestSuite
   , get
-  , mapKeys
+  , group
   , mkTestSuite
-  , nest
   , toTestTree
   ) where
 
+import           Data.Coerce (coerce)
 import           Data.List (partition)
-import           Data.List.NonEmpty (NonEmpty)
-import qualified Data.List.NonEmpty as NonEmpty
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import           Data.Universe.Class
+import           Data.Universe.Class (Finite (..), Universe (..))
+import           Data.Universe.Generic (GUniverse, universeGeneric)
+import           GHC.Generics (Generic (Rep))
 import           Ouroboros.Consensus.Block (BlockSupportsDiffusionPipelining,
                      ConvertRawHash, Header)
 import           Ouroboros.Consensus.Config.SupportsNode (ConfigSupportsNode)
-import           Ouroboros.Consensus.HardFork.Abstract
+import           Ouroboros.Consensus.HardFork.Abstract (HasHardForkHistory)
 import           Ouroboros.Consensus.Ledger.Basics (LedgerState)
 import           Ouroboros.Consensus.Ledger.Inspect (InspectLedger)
 import           Ouroboros.Consensus.Ledger.SupportsProtocol
@@ -34,9 +39,10 @@ import           Ouroboros.Consensus.Ledger.SupportsProtocol
 import           Ouroboros.Consensus.Storage.ChainDB (SerialiseDiskConstraints)
 import           Ouroboros.Consensus.Storage.LedgerDB.API
                      (CanUpgradeLedgerTables)
-import           Ouroboros.Consensus.Util.Condense
-import           Ouroboros.Network.Util.ShowProxy
-import           Test.Consensus.Genesis.Setup
+import           Ouroboros.Consensus.Util.Condense (Condense, CondenseList)
+import           Ouroboros.Network.Util.ShowProxy (ShowProxy)
+import           Test.Consensus.Genesis.Setup (ConformanceTest (..),
+                     runConformanceTest)
 import           Test.Consensus.PeerSimulator.StateView (StateView)
 import           Test.Consensus.PointSchedule (HasPointScheduleTestParams)
 import           Test.Consensus.PointSchedule.NodeState (NodeState)
@@ -44,50 +50,50 @@ import           Test.Tasty (TestTree, testGroup)
 import qualified Test.Tasty.QuickCheck as QC
 import           Test.Util.TersePrinting (Terse)
 
+newtype GenericUniverse a = GenericUniverse a
+
+instance (Generic a, GUniverse (Rep a)) => Universe (GenericUniverse a) where
+  universe = coerce $ universeGeneric @a
+
+instance (Generic a, GUniverse (Rep a)) => Finite (GenericUniverse a)
+
 data TestSuiteData blk = TestSuiteData
-  { -- | The nested groups a test belongs to.
-    -- It is 'NonEmpty', as we need a test description/name
-    -- in order to produce a 'TestTree'. For this, the
-    -- convention is to use the 'NonEmpty.last' and nest
-    -- the test by appending.
-    prefix :: NonEmpty Prefix
+  { -- | A prefix representing a path through the test group tree.
+    -- The convention is to nest by appending, i.e. the head of the prefix
+    -- corresponds to the top-level test group.
+    tsPrefix :: [String]
 
-    -- | The test to run.
-  , test   :: ConformanceTest blk
+    -- | The test itself.
+  , tsTest   :: ConformanceTest blk
   }
-
-type Prefix = String
 
 newtype TestSuite blk key = TestSuite (Map key (TestSuiteData blk))
 
--- | Smart constructor for a 'TestSuite'.
+-- | Build a 'TestSuite' from a mapping function.
 mkTestSuite :: (Ord key, Finite key)
-            => (key -> Prefix)
-            -> (key -> ConformanceTest blk)
+            => (key -> ConformanceTest blk)
             -> TestSuite blk key
-mkTestSuite toDescription toTest = TestSuite . Map.fromList $
-  [ (k, tsData)
-  | k <- universeF
-  , let tsData = TestSuiteData { prefix = [toDescription k]
-                               , test = toTest k
-                               }
-  ]
-
-mapKeys :: Ord b => (a -> b) -> TestSuite blk a -> TestSuite blk b
-mapKeys f (TestSuite m) = TestSuite $ Map.mapKeys f m
+mkTestSuite toConformanceTest = TestSuite . Map.fromList $ do
+  k <- universeF
+  let test = toConformanceTest k
+      tsData = TestSuiteData { tsPrefix = []
+                             , tsTest = test
+                             }
+  pure (k, tsData)
 
 get :: Ord key => TestSuite blk key -> key ->  ConformanceTest blk
-get (TestSuite m) k = test $ case Map.lookup k m of
+get (TestSuite m) k = tsTest $ case Map.lookup k m of
   Just t  -> t
-  Nothing -> error "TestSuite.get: Impossible! All test classes have a value by contruction."
+  Nothing -> error "TestSuite.get: Impossible! A TestSuite is a total map."
 
-nest :: [Prefix] -> TestSuite blk key -> TestSuite blk key
-nest pfs (TestSuite m) = TestSuite $
+-- | Appends the given string to the prefix of all tests.
+group :: String -> TestSuite blk key -> TestSuite blk key
+group pfs (TestSuite m) = TestSuite $
   Map.map (\testData ->
-             testData {prefix = NonEmpty.prependList pfs $ prefix testData}) m
+             testData {tsPrefix = pfs : tsPrefix testData}) m
 
--- | Produces a single test tasty 'TestTree', along with its containing
--- groups prefixes, out a 'TestSuiteData'.
+-- | Produces a single-test tasty 'TestTree', along with its containing
+-- 'group' prefixes, out a 'TestSuiteData'.
 compileSingleTest ::
   ( Condense (StateView blk)
   , CondenseList (NodeState blk)
@@ -107,45 +113,43 @@ compileSingleTest ::
   , Terse blk
   , Condense (NodeState blk)
   ) =>
-  TestSuiteData blk -> ([Prefix], TestTree)
-compileSingleTest (TestSuiteData {prefix, test}) =
-  -- The 'last' element of the 'prefix' corresponds to the individual test
+  TestSuiteData blk -> ([String], TestTree)
+compileSingleTest (TestSuiteData {tsPrefix, tsTest}) =
+  -- The 'last' element of the 'tcPrefix' corresponds to the individual test
   -- description/name. Conversely, the 'init' corresponds to the enclosing
   -- test groups.
-  let (prefixes,testName) = (NonEmpty.init prefix, NonEmpty.last prefix)
-   in (prefixes, QC.testProperty testName (runConformanceTest test))
+  let testName = ctDescription tsTest
+   in (tsPrefix, QC.testProperty testName (runConformanceTest tsTest))
 
 -- | Recursively build test groups by following the hierarchy described by
--- the '[Prefix]'. The resulting '[TestTree]' are considered to be part
+-- the '[String]'. The resulting '[TestTree]' are considered to be part
 -- of the same top level group.
-buildGroups :: [([Prefix], TestTree)] -> [TestTree]
+buildGroups :: [([String], TestTree)] -> [TestTree]
 buildGroups entries =
   let
     -- Distinguish between tests that belong to the top level
-    -- from the nested ones.
+    -- from the nested ones (i.e. those with a non-empty prefix).
     (toplevel, nested) = partition (null . fst) entries
 
     topLevelTests :: [TestTree]
     topLevelTests = fmap snd toplevel
 
     -- Group the nested tests by their first prefix.
-    grouped :: Map Prefix [([Prefix], TestTree)]
+    grouped :: Map String [([String], TestTree)]
     grouped =
-      Map.fromListWith (<>)
-        [ (p, [(ps, t)])
-        | (p:ps, t) <- nested
-        ]
+      Map.fromListWith (<>) $ do
+        (p:ps, t) <- nested
+        pure (p, [(ps, t)])
 
     subgroups :: [TestTree]
-    subgroups =
-      [ testGroup p (buildGroups xs)
-      | (p, xs) <- Map.toList grouped
-      ]
+    subgroups = do
+      (p, xs) <- Map.toList grouped
+      pure $ testGroup p (buildGroups xs)
   in
     topLevelTests <> subgroups
 
--- | Compile a 'TestSuite' into a tasty 'TestTree', with the given 'Prefix'
--- as the top level description/name.
+-- | Compile a 'TestSuite' into a tasty 'TestTree', with the given 'String'
+-- as the top level description/name of the resulting group.
 toTestTree ::
   ( Condense (StateView blk)
   , CondenseList (NodeState blk)
@@ -165,7 +169,7 @@ toTestTree ::
   , Terse blk
   , Condense (NodeState blk)
   ) =>
-  Prefix -> TestSuite blk key -> TestTree
+  String -> TestSuite blk key -> TestTree
 toTestTree p (TestSuite m) =
   let leafs = fmap snd $ Map.toAscList m
       entries = fmap compileSingleTest leafs
