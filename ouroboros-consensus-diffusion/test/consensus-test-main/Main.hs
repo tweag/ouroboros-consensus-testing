534f8d7ae9183f6a7e461a7414807ea8164ac792
Xavier Góngora <xavier.gongora@tweag.io>
2026-03-31 14:54:10 -0600

    Amend consensus-test-main and remove duplicate

--- ouroboros-consensus-diffusion/test/consensus-test-main/Main.hs
+++ ouroboros-consensus-diffusion/test/consensus-test-main/Main.hs
@@ -2,7 +2,7 @@ module Main (main) where
 
 import qualified Test.Consensus.BlockTree.Tests (tests)
 import qualified Test.Consensus.Genesis.Tests (tests)
-import qualified Test.Consensus.Genesis.TestSuite.SmallKey.Tests (tests)
+import qualified Test.Consensus.Genesis.TestSuite.Tests (tests)
 import qualified Test.Consensus.GSM (tests)
 import qualified Test.Consensus.HardFork.Combinator (tests)
 import qualified Test.Consensus.Node (tests)
@@ -36,5 +36,5 @@ tests =
     , Test.Consensus.PointSchedule.Tests.tests
     , Test.Consensus.BlockTree.Tests.tests
     , Test.Consensus.Serialize.Tests.tests
-    , Test.Consensus.Genesis.TestSuite.SmallKey.Tests.tests
+    , Test.Consensus.Genesis.TestSuite.Tests.tests
     ]
